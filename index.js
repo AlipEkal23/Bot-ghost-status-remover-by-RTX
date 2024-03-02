4f08c6e1f6adcce9223a393548899ef204f1083c
AlipEkal23 <157709691+AlipEkal23@users.noreply.github.com>
2024-03-03 05:32:03 +0800

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Listening to Spotify","Watching YouTube"];
+const statusMessages = ["Tengah Ngen","Tengah Tengok Server","Bacot Kontol"];
 
 
 let currentIndex = 0;
